cd723947d16914b7e3c6d7a30d0c210eba2ca030
_DEN_ <kononchukd04@gmail.com>
2021-11-26 02:50:45 +0300

    some test change

--- app/src/test/java/com/uni/DataOfWeatherTest.java
+++ app/src/test/java/com/uni/DataOfWeatherTest.java
@@ -8,11 +8,15 @@ import java.util.Date;
 import static org.junit.Assert.assertEquals;
 
 
-class DataOfWeatherTest {
+public class DataOfWeatherTest {
     DataOfWeather dataOfWeather;
 
+    public DataOfWeatherTest () {
+
+    }
+
     @Before
-    void setUp() {
+    public void setUp() {
         dataOfWeather = new DataOfWeather();
         dataOfWeather.setTime(new Date(1635840000 * 1000L));
         dataOfWeather.setHumidity(75);
@@ -23,32 +27,32 @@ class DataOfWeatherTest {
     }
 
     @Test
-    void getTemp() {
+    public void getTemp() {
         assertEquals(5.5, dataOfWeather.getTemp(), 0.0001);
     }
 
     @Test
-    void getFeelsLikeTemp() {
+    public void getFeelsLikeTemp() {
         assertEquals(5.5, dataOfWeather.getFeelsLikeTemp(), 0.0001);
     }
 
     @Test
-    void getWindSpeed() {
+    public void getWindSpeed() {
         assertEquals(1.2, dataOfWeather.getWindSpeed(), 0.0001);
     }
 
     @Test
-    void getPressure() {
+    public void getPressure() {
         assertEquals(1010, dataOfWeather.getPressure());
     }
 
     @Test
-    void getHumidity() {
+    public void getHumidity() {
         assertEquals(75, dataOfWeather.getHumidity());
     }
 
     @Test
-    void getTime() {
+    public void getTime() {
         Date expexted = new Date(1635840000 * 1000L);
         assertEquals(expexted, dataOfWeather.getTime());
     }
