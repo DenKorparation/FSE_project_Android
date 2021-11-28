6fdd66d855a14c2b16393d6ab157e470f3796abe
_DEN_ <kononchukd04@gmail.com>
2021-11-28 21:15:39 +0300

    add some test

--- app/src/test/java/com/uni/DataOfWeatherTest.java
+++ app/src/test/java/com/uni/DataOfWeatherTest.java
@@ -24,6 +24,10 @@ public class DataOfWeatherTest {
         dataOfWeather.setTemp((float)5.5);
         dataOfWeather.setWindSpeed((float)1.2);
         dataOfWeather.setFeelsLikeTemp((float)5.5);
+        dataOfWeather.setFeelsLikeTempDay((float)5.5);
+        dataOfWeather.setFeelsLikeTempNight((float)5.5);
+        dataOfWeather.setTempNight((float) 5.5);
+        dataOfWeather.setTempDay((float) 5.5);
     }
 
     @Test
@@ -55,5 +59,43 @@ public class DataOfWeatherTest {
     public void getTime() {
         Date expexted = new Date(1635840000 * 1000L);
         assertEquals(expexted, dataOfWeather.getTime());
+    }
+
+    @Test
+    public void setDescription() {
+        dataOfWeather.setDescription("ясно");
+        assertEquals("ясно", dataOfWeather.getDescription());
+    }
+
+    @Test
+    public void getFeelsLikeTempDay() {
+        assertEquals(5.5, dataOfWeather.getFeelsLikeTempDay(), 0.0001);
+    }
+
+    @Test
+    public void setFeelsLikeTempDay() {
+        dataOfWeather.setFeelsLikeTempDay((float) 10.0);
+        assertEquals(10.0, dataOfWeather.getFeelsLikeTempDay(), 0.0001);
+    }
+
+    @Test
+    public void getFeelsLikeTempNight() {
+        assertEquals(5.5, dataOfWeather.getFeelsLikeTempNight(), 0.0001);
+    }
+
+    @Test
+    public void setFeelsLikeTempNight() {
+        dataOfWeather.setFeelsLikeTempNight((float) 10.0);
+        assertEquals(10.0, dataOfWeather.getFeelsLikeTempNight(), 0.0001);
+    }
+
+    @Test
+    public void getTempDay() {
+        assertEquals(5.5, dataOfWeather.getTempDay(), 0.0001);
+    }
+
+    @Test
+    public void getTempNight() {
+        assertEquals(5.5, dataOfWeather.getTempNight(), 0.0001);
     }
 }
--- app/src/test/java/com/uni/DatabaseTest.java
+++ app/src/test/java/com/uni/DatabaseTest.java
@@ -3,20 +3,16 @@ package com.uni;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class DatabaseTest {
-    Database database;
-
-    public DatabaseTest()
-    {
-
-    }
+    Database database = new Database();
 
     @Before
     public void setUp() {
-        database = new Database();
         database.setNameOfCity("Minsk");
+        database.request();
     }
 
     @Test
@@ -34,10 +30,70 @@ public class DatabaseTest {
         assertNotNull(database.getDailyForecast());
     }
 
-    /*@Test
-    void request() throws Exception{
-        assertTimeout(ofSeconds(10000), () -> {
+    @Test(timeout = 10000)
+    public void request() throws Exception{
             database.request();
-        });
-    }*/
+    }
+
+    @Test
+    public void setNameOfCity() {
+        database.setNameOfCity("Minsk");
+        assertEquals("Minsk", database.getNameOfCity());
+    }
+
+    @Test
+    public void getNameOfCity() {
+        assertEquals("Минск", database.getNameOfCity());
+    }
+
+    @Test
+    public void isCorrectData() {
+        assertEquals(true, database.isCorrectData());
+    }
+
+    @Test
+    public void getPartOfDay() {
+        assertNotNull(database.getPartOfDay());
+    }
+
+    @Test
+    public void getCur_Condition() {
+        assertNotNull(database.getCur_Condition());
+    }
+
+    @Test
+    public void getMap() {
+        assertNotNull(database.getMap());
+    }
+
+    @Test
+    public void getWeatherMap() {
+        assertNotNull(database.getWeatherMap());
+    }
+
+    @Test
+    public void setMapLayer() throws Exception{
+        database.setMapLayer("temp_new");
+        database.reqMap();
+        assertNotNull(database.getMap());
+    }
+
+    @Test
+    public void zoomIncrement() throws Exception{
+        for(int i = 0; i < 13; i++){
+            database.zoomIncrement();
+        }
+    }
+
+    @Test
+    public void zoomDecrement() throws Exception{
+        for(int i = 0; i < 10; i++){
+            database.zoomDecrement();
+        }
+    }
+
+    @Test
+    public void getCodeOfCountry() {
+        assertEquals("BY", database.getCodeOfCountry());
+    }
 }
